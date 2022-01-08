06b03b37b12431bc284e3866ef48ecd597b5d7de
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-01-09 00:03:59 +0800

    wip: Adventure and Challenge added

--- slimegame/src/component/First.js
+++ slimegame/src/component/First.js
@@ -2,6 +2,7 @@ import "../css/Main.css"
 
 import {  Link } from "react-router-dom";
 import Slime_ABI from "../abi/Slime.json"
+import { useEffect } from "react";
 // import { useWeb3React } from "@web3-react/core"
 // import { InjectedConnector } from '@web3-react/injected-connector'
 // export const injected = new InjectedConnector({
@@ -16,22 +17,12 @@ const First = (account , web3)=> {
 
   // const { active, account, library, connector, activate, deactivate } = useWeb3React()
   // let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-
-  async function checkWalletIsConnected() {
-
-    if(!ethereum) {
-      console.log("Make sure your Metamask is installed!");
-      return;
-    }
-    else
-    {
-
-      console.log(account)
-      console.log("User is installed Metamask")
-      await window.ethereum.send('eth_requestAccounts');
-      console.log("User addr is : ",account.web3.eth.requestAccounts())
-    }
-  }
+  useEffect(() => {
+    console.log("Check account",account.account)
+    let Btn = document.getElementById("ConnectBtn");
+    Btn.style.display = 'block'
+  }, [])
+  
 
 
   async function check() {
@@ -55,22 +46,23 @@ const First = (account , web3)=> {
 
     return(
         <div className="First">
-        <div style={{width:"600px"}}>故事发生在2022年，
-神秘的Black Pearl团队建立了一个广袤的异世界——“初界”，
-那里有无数的探索式剧情、无限的打斗乐趣。
-于是你摩拳擦掌，满怀期待地开启了你的初界之旅，
-并成功获得了一个虚拟化身!!
-那就是……暴食伊塔!？</div>
+        <div style={{width:"1200px"}}>故事发生在2022年，<br/>
+                            神秘的Black Pearl团队建立了一个广袤的异世界——“初界”，<br/>
+                            那里有无数的探索式剧情、无限的打斗乐趣。<br/>
+                            于是你摩拳擦掌，满怀期待地开启了你的初界之旅， <br/>
+                            并成功获得了一个虚拟化身!! <br/>
+                            那就是……暴食伊塔!？</div>
 
 
-        <div className="flex flex-col items-center">
-        <button className="Btn2 mt-5" type="button" onClick={checkWalletIsConnected} > 连接钱包 </button>
-        <button className="Btn2  " type="button" onClick={check}> 查看用户资产 </button>
+         <div className="flex flex-col items-center">
+         {/* <Link to = "/start"> */}
 
-        <Link to = "/start">
-        <button className="Btn2 " type="button"> 点击进入初界 </button>
-        </Link>
-        </div>
+        {/* <button className="Btn2 mt-5" type="button" onClick={checkWalletIsConnected} > 连接钱包 </button> */}
+        {/* <button className="Btn2  " type="button" onClick={check}> 查看用户资产 </button> */}
+
+        {/* <button className="Btn2 " type="button"> 点击进入初界 </button> */}
+        {/* </Link> */}
+        </div> 
         </div>
 
     );
